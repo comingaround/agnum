e613b6515ea642f7f9c528fcb4a52e84cb0384d5
comingaround <131857839+comingaround@users.noreply.github.com>
2024-05-28 17:14:00 +0300

    commit

--- src/components/home/home_b2b/home_b2b.jsx
+++ src/components/home/home_b2b/home_b2b.jsx
@@ -8,6 +8,48 @@ export default function Home_b2b() {
                     <h2>B2B integracijos</h2>
                     <p>Kaip pradėti integracinį projektą?</p>
                 </div>
+                <div className="h_b2b_partners">
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/partners/Telema.jpg" alt="ss" />
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/partners/edisoft.jpg" alt="ss" />         
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/partners/Shopify.webp" alt="ss" />                       
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/partners/ivesk.png" alt="ss" />                      
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/partners/Prestashop.webp" alt="ss" />                                             
+                    </div>
+                </div>
+                <div className="h_b2b_disclaimer">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cum dolor iste ullam vitae maiores facere nostrum. Sint dolore optio debitis rem perspiciatis, eius mollitia cum laborum molestias voluptate magni nostrum necessitatibus expedita perferendis suscipit?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate pariatur animi est voluptatibus illum rem, ducimus repellat voluptates vitae voluptatum provident reiciendis odit nisi ab amet, at maiores nihil perferendis similique culpa minus excepturi? Officia quaerat, dicta animi velit voluptates nobis id voluptate nam nulla expedita officiis ab delectus. Totam maxime officia iure inventore et blanditiis obcaecati asperiores debitis dicta.</p>
+                    <button>Sužinoti daugiau</button>
+                </div>
+                <div className="h_b2b_title">
+                    <h2>Mus rekomenduoja</h2>
+                </div>
+                <div className="h_b2b_partners">
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/reko/moki.png" alt="ss" />
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/reko/dzukija.jpg" alt="ss" />         
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/reko/triumph.jpg" alt="ss" />                       
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/reko/diglita.png" alt="ss" />                      
+                    </div>
+                    <div className="h_b2bpartners_item">
+                        <img src="/images/reko/accounting.jpg" alt="ss" />                                             
+                    </div>
+                </div>
             </div>
         </>
     );
